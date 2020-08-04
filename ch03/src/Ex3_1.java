d1968af16a21b15b4b4c120d869a4e4b3ec50c61
whowho0820 <whowho0820@naver.com>
2020-08-04 03:06:00 -0400

    연습문제 3창

---
+++ ch03/src/Ex3_1.java
@@ -0,0 +1,11 @@
+class Ex3_1 {
+	public static void main(String[] args) {
+		int x, y;
+
+		x = y = 3; // y�� 3�� ����� �Ŀ�, x�� 3�� ����ȴ�.
+		System.out.println("x=" + x);
+		System.out.println("y=" + y);
+	}
+}
+
+//ㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐ
---
+++ ch03/src/exercise_3_3.java
@@ -0,0 +1,19 @@
+public class ex0_3_3 {
+    public static void main(String[] args) {
+        int x=2;
+        int y=5;
+        char c = 'A';
+        
+        System.out.println(1+x<<33);
+        System.out.println(y>=5||x<0&&x>2);
+        System.out.println(y+=10 - x++);
+        System.out.println(x+=2);
+        System.out.println(!('A'<=C&&C<='Z'));
+        System.out.println('C'-C);
+        System.out.println('5'-'0');
+        System.out.println(C+1);
+        System.out.println(++C);
+        System.out.println(C++);
+        System.out.println(C);
+    }
+}
