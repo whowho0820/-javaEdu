6302ee7fe297fcb438621c66f62f8f16c8d9b830
whowho0820 <whowho0820@naver.com>
2020-08-11 03:24:00 -0400

    정보시스템 이행 과제수행 중입니다.

--- ch03/src/Ex3_1.java
+++ ch03/src/Ex3_1.java
@@ -1,18 +1,11 @@
+
+	//정보시스템 이행 과제수행 중입니다. 
 class Ex3_1 {
 	public static void main(String[] args) {
 		int x, y;
-
-		x = y = 3; // y�� 3�� ����� �Ŀ�, x�� 3�� ����ȴ�.
+	
+		x = y = 3; 
 		System.out.println("x=" + x);
 		System.out.println("y=" + y);
 	}
 }
-
-//ㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐ
-
-
-//두번째수정
-
-//세번째수정
-
-//네번째수정
